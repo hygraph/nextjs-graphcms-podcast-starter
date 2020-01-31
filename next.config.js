3d49ab9cebdcc1fa69d4d76ff9f11b10d1812ea0
Jesse <themotleydev@gmail.com>
2020-01-31 23:29:28 +0100

    adding graphql endpoint

---
+++ next.config.js
@@ -0,0 +1,25 @@
+const {
+  PHASE_DEVELOPMENT_SERVER,
+  PHASE_PRODUCTION_BUILD
+} = require("next/constants");
+
+module.exports = phase => {
+  const isDev = phase === PHASE_DEVELOPMENT_SERVER;
+  const isProd =
+    phase === PHASE_PRODUCTION_BUILD && process.env.STAGING !== "1";
+  const isStaging = PHASE_PRODUCTION_BUILD && process.env.STAGING === "1";
+
+  const env = {
+    URL: (() => {
+      if (isDev) return "http://localhost:3000";
+      if (isProd) {
+        return "https://contentjazz.now.sh/";
+      }
+      if (isStaging) return "http://localhost:11639";
+    })()
+  };
+
+  return {
+    env
+  };
+};
---
+++ pages/api/graphql.js
@@ -0,0 +1,40 @@
+const { ApolloServer } = require("apollo-server-micro");
+const fetch = require("node-fetch");
+const { HttpLink } = require("apollo-link-http");
+const {
+  introspectSchema,
+  makeRemoteExecutableSchema
+} = require("graphql-tools");
+
+export const config = {
+  api: {
+    bodyParser: false
+  }
+};
+
+export default async (req, res, ...args) => {
+  if (req.method === "OPTIONS") return res.status(200).send();
+
+  const link = new HttpLink({
+    uri: process.env.GRAPHCMS_ENDPOINT,
+    useGETForQueries: true,
+    headers: {
+      Authorization: `Bearer ${process.env.GRAPHCMS_QUERY_TOKEN}`
+    },
+    fetch
+  });
+
+  const gcmsSchema = await introspectSchema(link);
+
+  const schema = makeRemoteExecutableSchema({
+    schema: gcmsSchema,
+    link
+  });
+
+  const apolloServer = new ApolloServer({
+    schema
+  });
+
+  const handler = apolloServer.createHandler({ path: "/api/graphql" });
+  return handler(req, res, ...args);
+};
